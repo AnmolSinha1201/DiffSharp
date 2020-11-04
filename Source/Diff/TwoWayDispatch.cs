521e2b88918cc53b71a8bcd7e002edab48ef2ecf
Anmol Sinha <anmolsinha1201@outlook.com>
2020-11-05 05:07:13 +0530

    Added TwoWayDispatches

---
+++ Source/Diff/TwoWayDispatch.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace DiffSharp
+{
+	public static partial class DiffGenerator
+	{
+		public static (List<Diff> Diff1, List<Diff> Diff2) TwoWayDiffs((object Object1, object Object2) Objects)
+		=> TwoWayDiffs(Objects.Object1, Objects.Object2);
+
+		public static (List<Diff> Diff1, List<Diff> Diff2) TwoWayDiffs(object Object1, object Object2)
+		{
+			var diff1 = SubsetDiffs(Object1, Object2);
+			var diff2 = SubsetDiffs(Object2, Object1);
+
+			return (diff1, diff2);
+		}
+
+
+		public static bool DeepEquals((object Object1, object Object2) Objects)
+		=> DeepEquals(Objects.Object1, Objects.Object2);
+
+		public static bool DeepEquals(object Object1, object Object2)
+		{
+			var diffs = TwoWayDiffs(Object1, Object2);
+			return diffs.Diff1.Count == 0 && diffs.Diff2.Count == 0;
+		}
+	}
+}
