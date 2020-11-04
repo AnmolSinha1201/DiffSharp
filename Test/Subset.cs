c980e5889a6788d8827f2f58439ea8022804ef74
Anmol Sinha <anmolsinha1201@outlook.com>
2020-11-05 04:55:24 +0530

    Added more tests

--- Test/Subset.cs
+++ Test/Subset.cs
@@ -35,5 +35,55 @@ namespace Test
 			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
 			Assert.Equal(0, diffs.Count);
 		}
+
+		[Fact]
+		public void ArrayComparison()
+		{
+			var json1 = new[] { 123, 456, 789 };
+			var json2 = new[] { 456, 123, 789 };
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
+
+		[Fact]
+		public void ListOfDictionaryComparison_DictionaryPartialMatch()
+		{
+			var json1 = "[{'qwe' : 'asd', 'zxc' : 123}, {'qwe' : 'asd', 'zxc' : 456}]".Deserialize();
+			var json2 = "[{'qwe' : 'asd', 'zxc' : 456}, {'qwe' : 'asd', 'zxc' : 123}]".Deserialize();
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
+
+		[Fact]
+		public void ListOfDictionaryComparison_DictionaryMissing()
+		{
+			var json1 = "[{'qwe' : 'asd', 'zxc' : 123}, {'qwe' : 'asd', 'zxc' : 456}]".Deserialize();
+			var json2 = "[{'qwe' : 'asd', 'zxc' : 456}]".Deserialize();
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(1, diffs.Count);
+		}
+
+		[Fact]
+		public void ListOfDictionaryWithListComparison()
+		{
+			var json1 = "[{'qwe' : 'asd', 'zxc' : 123, 'tyu' : [123, 456, 789]}, {'qwe' : 'asd', 'zxc' : 123}]".Deserialize();
+			var json2 = "[{'qwe' : 'asd', 'tyu' : [456, 123, 789], 'zxc' : 123}, {'qwe' : 'asd', 'zxc' : 123}]".Deserialize();
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
+
+		[Fact]
+		public void NullInListComparison()
+		{
+			var json1 = "[123, null, 789]".Deserialize();
+			var json2 = "[123, 789, null]".Deserialize();
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
 	}
 }
