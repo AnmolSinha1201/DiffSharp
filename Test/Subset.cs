ecdae1f5f62d51f4754a0fd6256a4202116ba419
Anmol Sinha <anmolsinha1201@outlook.com>
2020-11-05 04:00:29 +0530

    Added tests for simple list and dictionary

--- Test/Subset.cs
+++ Test/Subset.cs
@@ -7,10 +7,30 @@ namespace Test
 	public class UnitTest1
 	{
 		[Fact]
-		public void Test1()
+		public void StringComparison()
 		{
 			var json1 = "['qwe', 'asd', 'zxc']";
 			var json2 = "['qwe', 'asd', 'zxc']";
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
+
+		[Fact]
+		public void ListComparison()
+		{
+			var json1 = "['asd', 'qwe', 'zxc']".Deserialize();
+			var json2 = "['qwe', 'asd', 'zxc']".Deserialize();
+
+			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
+			Assert.Equal(0, diffs.Count);
+		}
+
+		[Fact]
+		public void DictionaryComparison()
+		{
+			var json1 = "{'qwe' : 'asd', 'zxc' : 123}".Deserialize();
+			var json2 = "{'zxc' : 123, 'qwe' : 'asd'}".Deserialize();
 
 			var diffs = DiffGenerator.SubsetDiffs(json1, json2);
 			Assert.Equal(0, diffs.Count);
