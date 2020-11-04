e429e04dcb583f30c5810f06867c939c35e2b2a0
Anmol Sinha <anmolsinha1201@outlook.com>
2020-11-05 02:47:43 +0530

    Added SubsetDiffs for Dictionary

---
+++ Source/Diff/Dictionary.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace DiffSharp
+{
+	public static partial class DiffGenerator
+	{
+		public static List<Diff> SubsetDiffs(Dictionary<object, object> Dictionary1, Dictionary<object, object> Dictionary2)
+		{
+			if (Dictionary1 == null || Dictionary2 == null)
+				throw new Exception("Cannot compare null kvps");
+
+			var dictionary2Keys = Dictionary2.Keys;
+			var retDiffs = new List<Diff>();
+
+			foreach (var kvp1 in Dictionary1)
+			{
+				var key2 = dictionary2Keys.DeepFindSubset(kvp1.Key);
+				var value1 = kvp1.Value;
+				var value2 = Dictionary2[key2];
+
+				var diffs = SubsetDiffs(value1, value2);
+				diffs.ForEach(i => i.Location.Add(kvp1.Key));
+
+				if (diffs.Count != 0)
+				{
+					retDiffs.AddRange(diffs);
+					continue;
+				}
+			}
+
+			return retDiffs;
+		}
+	}
+}
